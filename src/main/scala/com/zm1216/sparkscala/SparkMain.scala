f43e7b9bec09cf38bea50265ae8f6459b5577bcb
Zicong Ma <zm1216@ic.ac.uk>
2019-06-10 20:15:12 +0100

    try complex stuffs now

--- src/main/scala/com/zm1216/sparkscala/SparkMain.scala
+++ src/main/scala/com/zm1216/sparkscala/SparkMain.scala
@@ -155,34 +155,26 @@ object SparkMain{
         updates.toList.toIterator
     }
 
-    val query = herodf
-      .writeStream
-      .outputMode("update")
-      .format("console")
-      .start()
-
+    val (query, outputSchema) = new Stateful().TeamInfoAggregation(heroInfos, spark)
     query.awaitTermination(600000)
-
-//    val (query, outputSchema) = new MathCalculation().HeroPositionCalculation(heroInfos, spark)
-//    query.awaitTermination(1320000)
-//    query.stop()
-//    val realTimeMs = udf((t: java.sql.Timestamp) => t.getTime)
+    query.stop()
+    val realTimeMs = udf((t: java.sql.Timestamp) => t.getTime)
     println("\n THROUGHPUT FOR No Query \n" + numRecs * 1000 / (endTime - startTime) + "\n")
-//    spark.read
-//      .format("kafka")
-//      .option("kafka.bootstrap.servers", "localhost:9092")
-//      .option("subscribe", "output")
-//      .load()
-//      .withColumn("result", from_json('value.cast("string"), outputSchema))
-//      .select(col("result.eventTime").cast("timestamp") as "eventTime", 'timestamp)
-//      .select(realTimeMs('timestamp) - realTimeMs('eventTime) as 'diff)
-//      .selectExpr(
-//        "min(diff) as latency_min",
-//        "mean(diff) as latency_avg",
-//        "percentile_approx(diff, 0.95) as latency_95",
-//        "percentile_approx(diff, 0.99) as latency_99",
-//        "max(diff) as latency_max")
-//      .show()
+    spark.read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "output")
+      .load()
+      .withColumn("result", from_json('value.cast("string"), outputSchema))
+      .select(col("result.eventTime").cast("timestamp") as "eventTime", 'timestamp)
+      .select(realTimeMs('timestamp) - realTimeMs('eventTime) as 'diff)
+      .selectExpr(
+        "min(diff) as latency_min",
+        "mean(diff) as latency_avg",
+        "percentile_approx(diff, 0.95) as latency_95",
+        "percentile_approx(diff, 0.99) as latency_99",
+        "max(diff) as latency_max")
+      .show()
 
 
 //    val query = new MathCalculation().TerritoryControled(heroInfos, spark)
